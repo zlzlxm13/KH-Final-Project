307cd0af8fb4333671792fd2a074560d28ad906f
attention94 <attention94@naver.com>
2019-03-06 19:50:04 +0900

    controller

--- finalproject/src/main/java/controller/IndexController.java
+++ finalproject/src/main/java/controller/IndexController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
-
+//testsetset
 //http://localhost:8090/pet/index.do
 @Controller
 public class IndexController {
