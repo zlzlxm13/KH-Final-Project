f5b59102760bf0c447e2a6e88e53a6efa1793bf0
zlzlxm13 <48242628+zlzlxm13@users.noreply.github.com>
2019-03-19 19:08:27 +0900

    Update ReservationDAO.java

--- finalproject/src/main/java/dao/ReservationDAO.java
+++ finalproject/src/main/java/dao/ReservationDAO.java
@@ -7,7 +7,11 @@ import dto.ReservationDTO;
 
 public interface ReservationDAO {
 	
-	public void ReservationInsertMethod(ReservationDTO rdto);
+	public void save(ReservationDTO dto);
+	public List<ReservationDTO> search(String member_id);
+	public List<ReservationDTO> r_list();
+	public void r_delete(int num);
+	
 	
 	
 }//end interface
